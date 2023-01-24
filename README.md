012d7182c3e40c0c90aba349a16f1f91e0d73d57
sinhaapurva25 <sinhaapurva25@gmail.com>
2023-01-24 18:59:51 +0530

    adding requirements file

--- README.md
+++ README.md
@@ -4,4 +4,6 @@
 
 1. In line 16 of [face_recog.py](https://github.com/sinhaapurva25/face-recognition/blob/main/face_recog.py), the image path can be changed.
 
-2. Run the file [face_recog.py](https://github.com/sinhaapurva25/face-recognition/blob/main/face_recog.py) and results will be obtained.
+2. Open Command Prompt and do a "pip install -r requirements.txt".
+
+3. Run the file [face_recog.py](https://github.com/sinhaapurva25/face-recognition/blob/main/face_recog.py) and results will be obtained.
--- face_recog.py
+++ face_recog.py
@@ -1,34 +1,35 @@
 import numpy as np
 import cv2 as cv
 import os
-harr_cascade = cv.CascadeClassifier(r'haarcascades\haarcascade_frontalface_alt.xml')
 
-people = []
-for i in os.listdir(r'images'):
-    people.append(i)
+# harr_cascade = cv.CascadeClassifier(r'haarcascades\haarcascade_frontalface_alt.xml')
 
-# features = np.load(r'C:\Users\sinha\PycharmProjects\pythonProject\features.npy')
-# labels = np.load(r'C:\Users\sinha\PycharmProjects\pythonProject\labels.npy')
+# people = []
+# for i in os.listdir(r'images'):
+#     people.append(i)
 
-face_recognizer = cv.face.LBPHFaceRecognizer_create()
-face_recognizer.read(r'face_trained.yml')
+# # features = np.load(r'C:\Users\sinha\PycharmProjects\pythonProject\features.npy')
+# # labels = np.load(r'C:\Users\sinha\PycharmProjects\pythonProject\labels.npy')
 
-path = r'images\ben_affleck\2.jpg'
-img_color = cv.imread(path)
-img = cv.cvtColor(img_color, cv.COLOR_RGB2GRAY)
+# face_recognizer = cv.face.LBPHFaceRecognizer_create()
+# face_recognizer.read(r'face_trained.yml')
 
-# cv.imshow("person",img)
+# path = r'images\ben_affleck\2.jpg'
+# img_color = cv.imread(path)
+# img = cv.cvtColor(img_color, cv.COLOR_RGB2GRAY)
 
-faces_rect = harr_cascade.detectMultiScale(img, scaleFactor=1.1, minNeighbors=1)
-for(x, y, w, h) in faces_rect:
-    face_roi = img[y:y+h, x:x+w]
+# # cv.imshow("person",img)
 
-    label, confidence = face_recognizer.predict(face_roi)
-    print(f'label = {people[label]} with a confidence of {confidence}')
+# faces_rect = harr_cascade.detectMultiScale(img, scaleFactor=1.1, minNeighbors=1)
+# for(x, y, w, h) in faces_rect:
+#     face_roi = img[y:y+h, x:x+w]
 
-    cv.putText(img_color, str(people[label]),(20,20),cv.FONT_HERSHEY_COMPLEX, fontScale=0.4, color=(0,0,255), thickness=1)
-    cv.rectangle(img_color,(x,y),(x+w,y+h),(0,0,255),thickness=1)
+#     label, confidence = face_recognizer.predict(face_roi)
+#     print(f'label = {people[label]} with a confidence of {confidence}')
 
-cv.imshow("detected face",img_color)
-cv.waitKey(0)
-cv.destroyAllWindows()
+#     cv.putText(img_color, str(people[label]),(20,20),cv.FONT_HERSHEY_COMPLEX, fontScale=0.4, color=(0,0,255), thickness=1)
+#     cv.rectangle(img_color,(x,y),(x+w,y+h),(0,0,255),thickness=1)
+
+# cv.imshow("detected face",img_color)
+# cv.waitKey(0)
+# cv.destroyAllWindows()
